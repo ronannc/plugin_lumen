7010b173bfd5179b6a61f8618cfa1e1d9e5feef8
Ronan Nunes <ronan.nunes@prumoengenharia.com.br>
2021-05-24 23:59:50 -0300

    mudando registro do web routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,4 +3,5 @@
 /**
  * Rota de listagem de parcelas de venda
  */
-$this->app->get('plots-sale', 'ronannc\plugin_lumen\Http\Controllers\PlotsSaleControllers@index');
+Route::get( 'plots-sale', [ \ronannc\plugin_lumen\Http\Controllers\PlotsSaleControllers::class, 'index' ] );
+//$this->app->get('plots-sale', 'ronannc\plugin_lumen\Http\Controllers\PlotsSaleControllers');
