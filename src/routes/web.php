c27a2b81805ee3eb620520f1fc7057a5c62af7fd
Ronan Nunes <ronan.nunes@prumoengenharia.com.br>
2021-05-25 00:20:44 -0300

    modificando rota

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,5 @@
 /**
  * Rota de listagem de parcelas de venda
  */
-use Illuminate\Support\Facades\Route;
-
-Route::get( 'plots-sale', [ \ronannc\plugin_lumen\Http\Controllers\PlotsSaleControllers::class, 'index' ] );
+//Route::get( 'plots-sale', [ \ronannc\plugin_lumen\Http\Controllers\PlotsSaleControllers::class, 'index' ] );
+$this->app->get('plots-sale', 'ronannc\plugin_lumen\Http\Controllers\PlotsSaleControllers@index');
