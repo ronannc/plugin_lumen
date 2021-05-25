918b6eabde92b3ed4eb13b8590f70b4867f6ca43
Ronan Nunes <ronan.nunes@prumoengenharia.com.br>
2021-05-25 04:05:20 -0300

    params request

--- src/Http/Controllers/PlotsSaleControllers.php
+++ src/Http/Controllers/PlotsSaleControllers.php
@@ -11,10 +11,11 @@ class PlotsSaleControllers extends Controller
     /**
      * Função responsavel por listar as vendas e suas parcelas
      *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * @return mixed
      */
-    public static function index()
+    public static function index(\Illuminate\Http\Request $request)
     {
+        $params = $request->all();
         //recuperando as credenciais de acesso (client_code, client_key) configuradas no env
         $client_key  = env( 'CLIENT_KEY', 'FC-SB-15' );
         $client_code = env( 'CLIENT_CODE', '6ea297bc5e294666f6738e1d48fa63d2' );
@@ -26,11 +27,11 @@ class PlotsSaleControllers extends Controller
             'Client-Code'  => $client_code,
             'Client-key'   => $client_key,
         ] )->get( $base_uri . 'vendas', [
-            //                'nu_referencia' => $params['nu_referencia'] ?? null,
-            //                'nu_venda' => $params['nu_venda'] ?? null,
-            //                'page' => $params['page'] ?? null,
-            //                'per_page' => $params['per_page'] ?? null,
-            //                'dt_venda' => $params['dt_venda'] ?? null,
+            'nu_referencia' => $params['nu_referencia'] ?? null,
+            'nu_venda' => $params['nu_venda'] ?? null,
+            'page' => $params['page'] ?? null,
+            'per_page' => $params['per_page'] ?? null,
+            'dt_venda' => $params['dt_venda'] ?? null,
         ] )->json();
     }
 }
