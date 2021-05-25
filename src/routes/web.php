2575d83365af374585b44063f9baa33f6e32bfb9
Ronan Nunes <ronan.nunes@prumoengenharia.com.br>
2021-05-25 00:05:11 -0300

    inserindo facades route

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,5 +3,6 @@
 /**
  * Rota de listagem de parcelas de venda
  */
+use Illuminate\Support\Facades\Route;
+
 Route::get( 'plots-sale', [ \ronannc\plugin_lumen\Http\Controllers\PlotsSaleControllers::class, 'index' ] );
-//$this->app->get('plots-sale', 'ronannc\plugin_lumen\Http\Controllers\PlotsSaleControllers');
