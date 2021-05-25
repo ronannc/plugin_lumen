99090a1c6421b78860e974b62f64cab98f503899
Ronan Nunes <ronan.nunes@prumoengenharia.com.br>
2021-05-25 02:56:09 -0300

    colocando middleware na rota

--- src/PluginServiceProvider.php
+++ src/PluginServiceProvider.php
@@ -29,7 +29,7 @@ class PluginServiceProvider extends ServiceProvider
     {
         return [
             'prefix' => '',
-            'middleware' => ['web'],
+            'middleware' => '',
         ];
     }
 }
