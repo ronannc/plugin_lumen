2865a8a159f6b38b250c160607cebe9d472785f4
Ronan Nunes <ronan.nunes@prumoengenharia.com.br>
2021-05-25 03:20:11 -0300

    removendo bibliotecas

--- src/Http/Controllers/Controller.php
+++ src/Http/Controllers/Controller.php
@@ -2,12 +2,9 @@
 
 namespace Ronannc\PluginLumen\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Laravel\Lumen\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
 }
