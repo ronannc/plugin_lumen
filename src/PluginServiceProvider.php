a115dd355290b22bdb9dad0f7c346ce0eb61c40e
Ronan Nunes <ronan.nunes@prumoengenharia.com.br>
2021-06-10 12:48:24 -0300

    colocando arquivo de configuração

--- src/PluginServiceProvider.php
+++ src/PluginServiceProvider.php
@@ -18,9 +18,7 @@ class PluginServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-//        $this->publishes([
-//            __DIR__ . '/config/config.php' => $this->app->configPath('config.php'),
-//        ]);
+
     }
 
     /**
@@ -30,18 +28,19 @@ class PluginServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $configFolderPath = $this->app->basePath(self::CONFIG_FOLDER_NAME);
-
-        if (!is_dir($configFolderPath)) {
-            throw new FileNotFoundException("The config folder is missing.\nCreate it on the root folder of your project and add the config files there.");
-        }
-
-        collect(scandir($configFolderPath))->each(function ($file) {
-            $this->app->configure(basename($file, '.php'));
-        });
-
         //Register Our Package routes
         include __DIR__.'/routes/web.php';
+
+        $configPath = __DIR__.'/config/config.php';
+        $this->mergeConfigFrom($configPath, 'config');
+
+        $this->app->singleton('command.plugin_lumen.publish-config', function () {
+            return new PublishConfigCommand();
+        });
+
+        $this->commands(
+            'command.plugin_lumen.publish-config',
+        );
 
         // Let Laravel Ioc Container know about our Controller
         $this->app->make('Ronannc\PluginLumen\Http\Controllers\PlotsSaleControllers');
